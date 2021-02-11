54481049af3f05e4f4232ddc226a2c385f4d1b9c
Leo Cheung <lappangcheung@gmail.com>
2021-02-11 15:43:38 -0500

    Send, upload and store on firebase

--- src/components/Messages/MessageInput.js
+++ src/components/Messages/MessageInput.js
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 import { Segment, Input, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux'
+import { v4 as uuidv4 } from 'uuid'
 
 import firebase from '../../server/firebase'
+import ImageUpload from './ImageUpload'
 
 const MessageInput = (props) => {
 
     const messageRef = firebase.database().ref('messages')
+    const storageRef = firebase.storage().ref()
+
     const [messageState, setMessageState] = useState('')
+    const [fileDialogState, setFileDialogState] = useState(false)
 
     const createActionButtons = () => {
         return <>
-            <Button icon="send" onClick={onSubmit}/>
-            <Button icon="upload" />
+            <Button icon="send" onClick={() => { sendMessage() }}/>
+            <Button icon="upload" onClick={() => setFileDialogState(true)} />
         </>
     }
 
@@ -21,7 +26,7 @@ const MessageInput = (props) => {
         setMessageState(target.value)
     }
 
-    const createMessageInfo = () => {
+    const createMessageInfo = (downloadUrl) => {
         return {
             user: {
                 avatar: props.user.photoURL,
@@ -29,16 +34,32 @@ const MessageInput = (props) => {
                 id: props.user.uid
             },
             content: messageState,
+            image: downloadUrl || "",
             timestamp: firebase.database.ServerValue.TIMESTAMP
         }
     }
 
-    const onSubmit = () => {
-        if(messageState) {
+    const uploadImage = (file, contentType) => {
+        const filePath = `chat/images/${uuidv4()}.jpg`
+        storageRef
+            .child(filePath)
+            .put(file, {contentType: contentType})
+            .then(data => {
+                data.ref.getDownloadURL()
+                .then(url => {
+                    sendMessage(url)
+                })
+                .catch(err => console.log(err))
+            })
+            .catch(err => console.log(err))
+    }
+
+    const sendMessage = (downloadUrl) => {
+        if(messageState || downloadUrl) {
             messageRef
                 .child(props.channel.id)
                 .push()
-                .set(createMessageInfo())
+                .set(createMessageInfo(downloadUrl))
                 .then(() => setMessageState(''))
                 .catch(err => {
                     console.error(err)
@@ -55,6 +76,11 @@ const MessageInput = (props) => {
                 value={messageState}
                 label={createActionButtons()}
                 labelPosition="right"
+            />
+            <ImageUpload 
+                open={fileDialogState}
+                onClose={() => setFileDialogState(false)}
+                uploadImage={uploadImage}
             />
         </Segment>
     )
