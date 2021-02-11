b8c33bb5fbbc2f410e45ded189f26d9e200174df
Leo Cheung <lappangcheung@gmail.com>
2021-02-10 19:10:20 -0500

    Add signout function

--- src/components/Sidebar/UserInfo/UserInfo.js
+++ src/components/Sidebar/UserInfo/UserInfo.js
@@ -1,24 +1,54 @@
-import { Grid , Header, Icon, Image } from 'semantic-ui-react'
+import { Grid , Header, Icon, Image, Dropdown } from 'semantic-ui-react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+
+import firebase from '../../../server/firebase'
+
+const UserInfoGridRow = styled(Grid.Row)`
+    padding: 1.2rem;
+`
+
+const UserInfoHeaderDisplay = styled(Header)`
+    padding: 1.2rem;
+`
 
 const UserInfo = props => {
 
+    const getDropDownOptions = () => {
+        return [{
+            key: 'signout',
+            text: <span onClick={signOut}>Sign Out</span>
+        }]
+    }
+
+    const signOut = () => {
+        firebase
+            .auth()
+            .signOut()
+            .then(() => console.log("user has signed out"))
+    }
+    
     if (props.user){
         return(
             <Grid>
                 <Grid.Column>
-                    <Grid.Row>
+                    <UserInfoGridRow>
                         <Header inverted as="h2">
                             <Icon name="slack"/>
                             <Header.Content>Slack</Header.Content>
                         </Header>
-                        <Header inverted as="h4">
-                            <span>
-                                <Image src={props.user.photoURL} avatar/>
-                                {props.user.displayName}
-                            </span>
-                        </Header>
-                    </Grid.Row>
+                        <UserInfoHeaderDisplay inverted as="h4">
+                            <Dropdown
+                                trigger={
+                                    <span>
+                                        <Image src={props.user.photoURL} avatar/>
+                                        {props.user.displayName}
+                                    </span>
+                                }
+                                options={getDropDownOptions()}
+                            />
+                        </UserInfoHeaderDisplay>
+                    </UserInfoGridRow>
                 </Grid.Column>
             </Grid>
         )
