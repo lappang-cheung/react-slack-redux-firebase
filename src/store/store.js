57968b7ad8df3cc579011d80cf5b744460367876
lappang-cheung <lappangcheung@gmail.com>
2021-02-13 19:32:21 -0500

    Add redux tools integration

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
 import { createStore } from "redux";
 import { combinedReducers } from "./reducers";
 
-export const store = createStore(combinedReducers);
+export const store = createStore(combinedReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
